9cd8bae9e44aea8f30c9c262628cab64c356c073
pbc1017 <pbc1017@kaist.ac.kr>
2023-08-09 00:28:46 +0900

    이제 알겠지?

--- front/src/routes/RoutesSetup.tsx
+++ front/src/routes/RoutesSetup.tsx
@@ -1,11 +1,13 @@
 import {Routes, Route} from 'react-router-dom'
 // import {Login} from '/pages/Login/'
+import { PhotoEmpty } from 'components/PhotoEmpty'
 import Home from './Home'
 
 export default function RouteSetup() {
     return (
         <Routes>
             <Route path= "/" element = {<Home/>}/>
+            <Route path= "/photo" element = {<PhotoEmpty/>}/>
             {/* <Route path= "/login" element = {<Login/>}/> */}
         </Routes>
     )
