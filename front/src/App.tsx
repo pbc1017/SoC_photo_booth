3cc7a9ce6318d94ff933ecd7649c93cb9d4a94f5
pbc1017 <pbc1017@kaist.ac.kr>
2023-08-09 00:16:20 +0900

    dev home

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,25 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect }  from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import RouteSetup from 'routes/RoutesSetup';
 
-function App() {
+const App: React.FC = () => {
+  // localStorage.removeItem('userProfile');
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <RouteSetup/>
+    </BrowserRouter>
   );
 }
 
---
+++ front/src/pages/Home/Home.tsx
@@ -0,0 +1,9 @@
+
+export const Home = ({
+
+}): JSX.Element => {
+  return (
+    <div>
+    </div>
+  );
+};
