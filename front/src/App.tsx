3ea03b23cc549463011557a43c582cb0b6d36f15
Yoo KyeongMi <yookyungmi02@gmail.com>
2023-08-17 19:57:17 +0900

    널 처리

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect }  from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import RouteSetup from 'routes/RoutesSetup';
+import Modal from "react-modal";
 
 const App: React.FC = () => {
   // localStorage.removeItem('userProfile');
--- front/src/pages/Loading/Loading.tsx
+++ front/src/pages/Loading/Loading.tsx
@@ -1,9 +1,11 @@
-import React, { useState } from "react";
+import React, { useState ,useRef,useEffect} from "react";
 import { Button } from "components/Button";
 import { useNavigate, useLocation } from "react-router-dom"; // useLocation 추가
 import * as AWS from 'aws-sdk';
-import { S3 } from 'aws-sdk';
+import toast, { Toaster } from 'react-hot-toast';
 import "./style.css";
+import { NotifyModal } from "components/NotifyModal";
+
 
 AWS.config.update({
     region: 'ap-northeast-2', 
@@ -12,7 +14,32 @@ AWS.config.update({
 
 const s3 = new AWS.S3();
 
+type InputState = {
+  value: string;
+  isValid: boolean;
+};
+
 export const Loading = (): JSX.Element => {
+  // modal 설정
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+  const [warningMessage, setWarningMessage] = useState<string>("");
+  
+  const handleAlert = (message: string): void => {
+    setWarningMessage(message);
+    setIsModalOpen(true);
+  };
+  let closeModal = (): void => {
+    setIsModalOpen(false);
+  };
+  const closeAndMove = (): void => {
+    setIsModalOpen(false);
+    navigate("/result", {state: {imageSrc}});
+  };
+  
+  useEffect(() => {
+    handleAlert("인쇄 할 거면 인쇄 누르고 안할거면 건너뛰기 누르세요\n 인쇄 하려면 학번 이름 필수");  // 수정하자
+  }, []);
+
   let vh = window.innerHeight * 0.01;
   document.documentElement.style.setProperty("--vh", `${vh}px`);
   window.addEventListener("resize", () => {
@@ -42,6 +69,10 @@ export const Loading = (): JSX.Element => {
   };
 
   const handlePrintClick = async () => {
+    // if (studentNumber.value === "" || name.value === "") {
+    //   handleAlert("학번 이름 필수");  // 수정하자
+    //   return;
+    // }
     // 이미지를 Blob으로 가져옴
     const response = await fetch(imageSrc);
     const blob = await response.blob();
@@ -50,10 +81,9 @@ export const Loading = (): JSX.Element => {
     const fileName = studentNumber.toString() + '_' + name + '.jpg';
   
     // Blob을 S3로 전송
-    await uploadBlobToS3(blob, fileName);
-  
-    // "/result" 경로로 이동하면서 imageSrc 전달
-    navigate("/result", { state: { imageSrc } });
+    //await uploadBlobToS3(blob, fileName);
+    await asyncAlert("인쇄 완료");
+    navigate("/result", {state: {imageSrc}});
   };
 
   const handleSkipClick = () => {
@@ -61,30 +91,77 @@ export const Loading = (): JSX.Element => {
     navigate("/result", { state: { imageSrc } });
   };
 
-  const [studentNumber, setStudentNumber] = useState("");
-  const [name, setName] = useState("");
+  const asyncAlert = (message:string) => {
+    return new Promise((resolve, reject) => {
+      handleAlert(message);
+      const originalClose = closeModal;
+      closeModal = () => {
+        originalClose();
+        resolve(1);
+      }
+    });
+  }
+  const [studentNumber, setStudentNumber] = useState<InputState>({ value: "", isValid: true });
+  const [name, setName] = useState<InputState>({ value: "", isValid: true });
+
+  
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>, type: "studentNumber" | "name") => {
+    const newValue = e.target.value;
+    let isValid = false;
+
+    if (type === "studentNumber") {
+      isValid = /^[0-9]*$/.test(newValue);
+      if (isValid) {
+        setStudentNumber({ value: newValue, isValid });
+      }
+      else {
+        toast.error(
+          "학번은 숫자만 입력할 수 있습니다.",
+          {duration: 1500}
+        )
+      }
+    } 
+    else if (type === "name") {
+      isValid =  /^[\p{L}\s]*$/u.test(newValue);
+      if (isValid) {
+        setName({ value: newValue, isValid });
+      }
+      else{
+        toast.error(
+          "이름은 한글 또는 영문만 입력할 수 있습니다.",
+          {duration: 1500}
+        )
+      }
+    }
+  };
 
   return (
     <div className="home">
-      <div className="div">
-        <input
-          type="text"
-          placeholder="학번을 입력하세요"
-          value={studentNumber}
-          onChange={(e) => setStudentNumber(e.target.value)}
-          className="input-student-number"
-        />
-        <input
-          type="text"
-          placeholder="이름을 입력하세요"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          className="input-name"
-        />
-        <Button className="button-1" text="인쇄하기" onClick={handlePrintClick} />
-        <Button className="button-2" text="건너뛰기" onClick={handleSkipClick}/>
-        <img className="element" alt="Element" src={imageSrc} /> {/* 넘겨받은 이미지 src 사용 */}
-      </div>
+    <div className="div">
+    <input
+    type="text"
+    placeholder="학번을 입력하세요"
+    value={studentNumber.value}
+    onChange={(e) =>  handleInputChange(e, "studentNumber")}
+    />
+    <input
+    type="text"
+    placeholder="이름을 입력하세요"
+    value={name.value}
+    onChange={(e) =>  handleInputChange(e, "name")}
+    />
+    
+    <Toaster/>
+    <Button className="button-1" text="인쇄하기" onClick={handlePrintClick} />
+    <Button className="button-2" text="건너뛰기" onClick={handleSkipClick}/>
+    <img className="element" alt="Element" src={imageSrc} /> {/* 넘겨받은 이미지 src 사용 */}
+    </div>
+    <NotifyModal
+              isOpen={isModalOpen}
+              onRequestClose={(closeModal)}
+              message={warningMessage}
+            />
     </div>
   );
+  
 };
