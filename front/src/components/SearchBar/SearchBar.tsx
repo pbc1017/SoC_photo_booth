e8ca5ccae52da5960e4be0b26f06806a41c70468
pbc1017 <pbc1017@kaist.ac.kr>
2023-09-01 07:43:01 +0900

    admin

--- front/src/components/SearchBar/SearchBar.tsx
+++ front/src/components/SearchBar/SearchBar.tsx
@@ -1,39 +1,58 @@
 import React, { useState } from 'react';
 import "./index.css";
 import {Button} from "components/Button";
-interface Props {
-  
-}
+import AWS from 'aws-sdk';
 
-export const SearchBar : React.FC<Props> = ({}) => {
+interface Props {}
+
+export const SearchBar : React.FC<Props> = () => {
   
   const [searchResults, setSearchResults] = useState<string[]>([]);
-  const [photoImages, setPhotoImages] = useState<string[]>([]); // 내부 상태로 추가
+  const [selectedImage, setSelectedImage] = useState<string | null>(null);
 
   const handleSearchClick = async () => {
+    setSelectedImage(null);
     const searchTerm = (document.querySelector('.SearchInput') as HTMLInputElement).value;
 
-    // 검색어를 서버로 보내고 검색 결과를 받아옵니다.
+    // AWS 설정
+    AWS.config.update({
+      region: 'ap-northeast-2',
+      accessKeyId: 'AKIASXX4F2YI6VYLYLNU',
+      secretAccessKey: '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'
+    });
+    
+    const s3 = new AWS.S3();
     try {
-      const response = await fetch(`http://localhost/api/search?term=${searchTerm}`);
-      const searchData = await response.json();
-      setSearchResults(searchData.results); // 서버에서 받은 검색 결과를 상태로 설정
+      const data = await s3.listObjectsV2({ Bucket: 'soc-photo' }).promise();
+      const filteredResults = (data.Contents?.filter(item => item.Key?.includes(searchTerm)).map(item => item.Key).filter(Boolean) || []) as string[];
+      setSearchResults(filteredResults); 
     } catch (error) {
-      console.error('검색 중 오류 발생:', error);
+      console.error('S3 검색 중 오류 발생:', error);
     }
   };
+
+  const handleImageNameClick = (imageName: string) => {
+    setSelectedImage(imageName);
+  };
+
   return(
     <div className='searchbar'>
-      <input type="text" className="SearchInput" />
+      <input type="text" className="SearchInput" placeholder="이름을 입력하세요"/>
       <Button className="button" text="찾기" onClick={handleSearchClick} />
       <div className='search-results'>
         {searchResults.map((result, index) => (
-          <img key={index} src={result} alt={`Result ${index}`} />
+          <p key={index} onClick={() => handleImageNameClick(result)}>
+            {result}
+          </p>
         ))}
       </div>
+      {selectedImage && 
+        <div className='selected-image'>
+          <img className = 'selected-img' src={`https://soc-photo.s3.ap-northeast-2.amazonaws.com/${selectedImage}`} alt={`Selected ${selectedImage}`} />
+        </div>
+      }
     </div>
   );
-
 };
 
 export default SearchBar;
